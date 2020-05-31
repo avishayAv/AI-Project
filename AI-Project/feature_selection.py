b3d2f82b829c3f43715db6dea0cb97e9b7559e9b
yakir-yehuda <yakirph@gmail.com>
2020-05-31 17:57:27 +0300

    change from max to min

--- AI-Project/feature_selection.py
+++ AI-Project/feature_selection.py
@@ -19,12 +19,12 @@ def selectKBest_features_selection(x: pd.DataFrame, y: pd.DataFrame, k):
 def findKBest_features_selection(x:pd.DataFrame,y:pd.DataFrame,num_features):
     split = 5
     kf = KFold(n_splits=split)
-    max_svm_features = []
-    max_svm= 0
-    max_forest_features = []
-    max_forest = 0
-    max_knn_features = []
-    max_knn= 0
+    min_svm_features = []
+    min_svm= np.inf
+    min_forest_features = []
+    min_forest = np.inf
+    min_knn_features = []
+    min_knn= np.inf
     for i in range(1,num_features):
         print(i)
         features = selectKBest_features_selection(x, y, i)
@@ -35,19 +35,19 @@ def findKBest_features_selection(x:pd.DataFrame,y:pd.DataFrame,num_features):
         result = dict(functools.reduce(operator.add,map(collections.Counter,tmp)))
         for key in result:
             result[key] = result[key]/split
-        if result['svm'] > max_svm:
-            max_svm = result['svm']
-            max_svm_features = features
-        if result['forest'] > max_forest:
-            max_forest = result['forest']
-            max_forest_features = features
-        if result['knn'] > max_knn:
-            max_knn = result['knn']
-            max_knn_features = features
-    print("max_svm = {} ,max_forest = {} , max_knn = {}".format(max_svm,max_forest,max_knn))
-    print("svm_featues = {}, size = {}".format(max_svm_features,len(max_svm_features)))
-    print("forest_featues = {}, size = {}".format(max_forest_features,len(max_forest_features)))
-    print("knn_featues = {}, size = {}".format(max_knn_features,len(max_knn_features)))
+        if result['svm'] < min_svm:
+            min_svm = result['svm']
+            min_svm_features = features
+        if result['forest'] < min_forest:
+            min_forest = result['forest']
+            min_forest_features = features
+        if result['knn'] < min_knn:
+            min_knn = result['knn']
+            min_knn_features = features
+    print("min_svm = {} ,min_forest = {} , min_knn = {}".format(min_svm,min_forest,min_knn))
+    print("svm_featues = {}, size = {}".format(min_svm_features,len(min_svm_features)))
+    print("forest_featues = {}, size = {}".format(min_forest_features,len(min_forest_features)))
+    print("knn_featues = {}, size = {}".format(min_knn_features,len(min_knn_features)))
 
 
 
@@ -68,24 +68,24 @@ def ExtraTree_feature_selection(data_X, data_Y, k):
         #print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
 
     # Plot the feature importances of the forest
-    plt.figure()
+    """plt.figure()
     plt.title("Feature importances")
     plt.bar(range(X.shape[1]), importances[indices],
             color="r", yerr=std[indices], align="center")
     plt.xticks(range(X.shape[1]), indices)
     plt.xlim([-1, X.shape[1]])
-    plt.show()
+    plt.show()"""
     return indices[:k]
 
 def findKExtraTree_feature_selection(x, y, num_features):
     split = 5
     kf = KFold(n_splits=split)
-    max_svm_features = []
-    max_svm = 0
-    max_forest_features = []
-    max_forest = 0
-    max_knn_features = []
-    max_knn = 0
+    min_svm_features = []
+    min_svm = np.inf
+    min_forest_features = []
+    min_forest = np.inf
+    min_knn_features = []
+    min_knn = np.inf
     for i in range(1, num_features):
         print(i)
         features_indices = ExtraTree_feature_selection(x, y, i)
@@ -97,18 +97,18 @@ def findKExtraTree_feature_selection(x, y, num_features):
         result = dict(functools.reduce(operator.add, map(collections.Counter, tmp)))
         for key in result:
             result[key] = result[key] / split
-        if result['svm'] > max_svm:
-            max_svm = result['svm']
-            max_svm_features = features
-        if result['forest'] > max_forest:
-            max_forest = result['forest']
-            max_forest_features = features
-        if result['knn'] > max_knn:
-            max_knn = result['knn']
-            max_knn_features = features
-    print("max_svm = {} ,max_forest = {} , max_knn = {}".format(max_svm, max_forest, max_knn))
-    print("svm_featues = {}, size = {}".format(max_svm_features, len(max_svm_features)))
-    print("forest_featues = {}, size = {}".format(max_forest_features, len(max_forest_features)))
-    print("knn_featues = {}, size = {}".format(max_knn_features, len(max_knn_features)))
+        if result['svm'] < min_svm:
+            min_svm = result['svm']
+            min_svm_features = features
+        if result['forest'] < min_forest:
+            min_forest = result['forest']
+            min_forest_features = features
+        if result['knn'] < min_knn:
+            min_knn = result['knn']
+            min_knn_features = features
+    print("min_svm = {} ,min_forest = {} , min_knn = {}".format(min_svm, min_forest, min_knn))
+    print("svm_featues = {}, size = {}".format(min_svm_features, len(min_svm_features)))
+    print("forest_featues = {}, size = {}".format(min_forest_features, len(min_forest_features)))
+    print("knn_featues = {}, size = {}".format(min_knn_features, len(min_knn_features)))
 
 
